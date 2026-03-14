3ea7072f23dd1c11d8ab3f0c9d6470776528fdfd
Jim McKeeth <jim@mckeeth.org>
2026-03-13 21:05:39 -0600

    header update

--- src/BigPi.pas
+++ src/BigPi.pas
@@ -1,10 +1,10 @@
 ﻿{===============================================
 
- Delphi Pi — Computing Pi in Delphi
+ Delphi Big Pi — Computing Pi in Delphi
  https://github.com/jimmckeeth/DelphiPi
 
  Licensed under GNU General Public License v3.0 (GPLv3)
- Copyright © 2025 Jim McKeeth
+ Copyright © 2023-2026 by James McKeeth
 
  Uses Rudy's Big Numbers Library (BSD 2-Clause)
  https://github.com/TurboPack/RudysBigNumbers
